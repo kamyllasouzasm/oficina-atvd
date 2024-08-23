01b6775bc8133e849868289075f54fe59b9e5922
kamyllasouzasm <145375104+kamyllasouzasm@users.noreply.github.com>
2024-08-23 17:53:07 -0300

    game over att

--- PROJETO OFICINA/Assets/scripts/GameController.cs
+++ PROJETO OFICINA/Assets/scripts/GameController.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {  
 
     public int totalScore;
     public TextMeshProUGUI scoreText;
+
+    public GameObject gameOver;
 
     public static GameController instance;
 
@@ -22,5 +25,13 @@ public class GameController : MonoBehaviour
         scoreText.text = totalScore.ToString();
     }
 
- 
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName);
+    {
+        SceneManager.LoadScene(lvlName);
+    }
 }
--- PROJETO OFICINA/Assets/scripts/Player.cs
+++ PROJETO OFICINA/Assets/scripts/Player.cs
@@ -80,6 +80,12 @@ public class NewBehaviourScript : MonoBehaviour
             isJumping = false;
             anim.SetBool("jump", false);
         }
+
+        if(collision.gameObject.tag == "Spike")
+        {
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionExit2D(Collision2D collision)
